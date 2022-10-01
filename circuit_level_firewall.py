a8ddc7994eb7b0396c4b94da67113713234a9113
Chinmay1292 <chinmaychougule1212@gmail.com>
2022-10-01 08:37:53 +0530

    choices

--- circuit_level_firewall.py
+++ circuit_level_firewall.py
@@ -1,3 +1,8 @@
 from datetime import datetime, timedelta
 import time
 import logo
+
+print('Choose 1 for Blocking sites and 2 for Unblocking: \n')
+
+print('1. Blocking. \n2. Unblocking')
+choice = int(input("\nMake your choice 1 or 2: "))
